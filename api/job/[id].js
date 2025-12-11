12832b67c7c589561e7b4025a30968310b45864f
Pupartai <woof@pupartai.com>
2025-12-11 07:40:42 +0100

    Update [id].js

--- api/job/[id].js
+++ api/job/[id].js
@@ -1,5 +1,17 @@
 // api/job/[id].js
+import { getJob } from "../../../lib/jobQueue.js";
+
 export default async function handler(req, res) {
   const { id } = req.query;
-  return res.status(200).json({ message: `Job ${id} endpoint is live.` });
+
+  const job = getJob(id);
+
+  if (!job) {
+    return res.status(404).json({ error: "Job not found" });
+  }
+
+  return res.status(200).json({
+    jobId: job.id,
+    state: job.state
+  });
 }
