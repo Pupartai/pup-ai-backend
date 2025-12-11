c027ec168fa919adbfad9d9617824ac056d217e9
Pupartai <woof@pupartai.com>
2025-12-11 09:39:36 +0100

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -1,8 +1,19 @@
 // api/generate.js
-import { createJob } from "./_lib/jobs";
+
+// Very simple in-memory job store
+const jobs = global.jobs || (global.jobs = {});
 
 export default function handler(req, res) {
-  const id = createJob();
+  // Create job ID
+  const id = Math.random().toString(36).substring(2, 10);
+
+  // Save job
+  jobs[id] = { id, state: "processing" };
+
+  // Simulate finishing after 2 seconds
+  setTimeout(() => {
+    jobs[id].state = "finished";
+  }, 2000);
 
   return res.status(200).json({
     id,
