97b994cb4cf1a7f331b6d60ac30a78e7dcebb409
Pupartai <woof@pupartai.com>
2025-12-11 09:01:15 +0100

    Update job.js

--- api/job.js
+++ api/job.js
@@ -1,21 +1,6 @@
-import { getJob } from "./_lib/jobQueue";
-
 export default function handler(req, res) {
-  try {
-    const { id } = req.query;
-
-    const job = getJob(id);
-
-    return res.status(200).json({
-      ok: true,
-      received: id,
-      job,
-    });
-  } catch (err) {
-    console.error("JOB ENDPOINT ERROR:", err);
-    return res.status(500).json({
-      ok: false,
-      error: err.message
-    });
-  }
+  return res.status(200).json({
+    ok: true,
+    msg: "job endpoint works"
+  });
 }
