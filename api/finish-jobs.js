03757850f14540c68da31b6408409e1bfb73d5b0
Pupartai <woof@pupartai.com>
2025-12-11 18:37:31 +0100

    Update finish-jobs.js

--- api/finish-jobs.js
+++ api/finish-jobs.js
@@ -3,8 +3,9 @@ import { allowCors } from "./_lib/cors.js";
 import { kv } from "@vercel/kv";
 
 export default async function handler(req, res) {
-  // Enable CORS
-  allowCors(res);
+  res.setHeader("Access-Control-Allow-Origin", "https://pupartai.com");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   if (req.method === "OPTIONS") {
     return res.status(200).end();
