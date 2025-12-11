9423707fe1c2532171e1c06d5c561f3eba359faf
Pupartai <woof@pupartai.com>
2025-12-11 09:17:31 +0100

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -1,19 +1,6 @@
-// api/generate.js
-
-// Import the same mock job storage (copy/paste inside this file)
-const jobs = {};
+import { createJob } from "./_lib/jobs";
 
 export default function handler(req, res) {
-  // Create a fake job ID
-  const id = Math.random().toString(36).substring(2, 10);
-
-  // Store job in memory
-  jobs[id] = { id, state: "processing" };
-
-  // Simulate job finishing after 2 seconds
-  setTimeout(() => {
-    jobs[id].state = "finished";
-  }, 2000);
-
-  return res.status(200).json({ id, state: "processing" });
+  const job = createJob();
+  return res.status(200).json(job);
 }
