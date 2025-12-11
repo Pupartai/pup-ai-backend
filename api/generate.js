19c203506b28530f962ab8b5067a517039b67d6b
Pupartai <woof@pupartai.com>
2025-12-11 18:35:21 +0100

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -2,8 +2,7 @@
 import { createJob } from "./_lib/jobs";
 
 export default async function handler(req, res) {
-  // --- CORS ---
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "https://pupartai.com");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
