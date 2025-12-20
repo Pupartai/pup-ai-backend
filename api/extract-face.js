6b9267ae53f06d6fd67e608444bf63fd257c26e4
Pupartai <woof@pupartai.com>
2025-12-20 16:39:09 +0100

    Update extract-face.js

--- api/extract-face.js
+++ api/extract-face.js
@@ -24,12 +24,12 @@ export default async function handler(req, res) {
       return res.status(400).json({ error: "Missing imageUrl" });
     }
 
-    const output = await replicate.run(
-      "zsxkib/bi-refnet",
-      {
-        input: { image: imageUrl }
-      }
-    );
+  const output = await replicate.run(
+    "cjwbw/rembg",
+    {
+      input: { image: imageUrl }
+    }
+  );
 
     return res.status(200).json({ facePng: output });
   } catch (err) {
